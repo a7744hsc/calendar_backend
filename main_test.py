5423489862f409759df9a606923a9c64dc9ef71b
hchan <hchan@thoughtworks.com>
2017-03-08 23:32:45 +0800

    add test.db into gitignore to freeze test db. refine the test  class

--- main_test.py
+++ main_test.py
@@ -1,19 +1,22 @@
 from main import app as main_app
 import unittest
 import base64, json
-import os,sys
-import tempfile
+import os
+from shutil import copyfile
 
 
 class FlaskTestCase(unittest.TestCase):
     def setUp(self):
         # self.db_fd, main_app.config['DATABASE'] = tempfile.mkstemp()
-        main_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+        copyfile('test.db', 'temp.db')
+        main_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///temp.db'
         self.app = main_app.test_client()
         # with main_app.app_context():
         #     flaskr.init_db()
 
     def tearDown(self):
+        os.remove('temp.db')
         pass
         # os.close(self.db_fd)
         # os.unlink(flaskr.app.config['DATABASE'])
@@ -21,8 +24,8 @@ class FlaskTestCase(unittest.TestCase):
     # Ensure that we can get all events correctly
     def test_get_all_events(self):
         response = self.app.get('/calendar/v1.0/events', headers={
-                'Authorization': 'Basic ' + base64.b64encode(bytes("user:passwd", 'utf-8')).decode('utf-8')
-            })
+            'Authorization': 'Basic ' + base64.b64encode(bytes("user:passwd", 'utf-8')).decode('utf-8')
+        })
         response_str = response.data.decode('unicode_escape')
         res_obj = json.loads(response_str)
         # c = a.decode('utf-8')
@@ -31,7 +34,7 @@ class FlaskTestCase(unittest.TestCase):
         # d= a.decode('big5')
         # d = a.decode('utf-16')
         # sys.stdout.buffer.write(a)
-        getattr(res_obj,'id',None)
+        getattr(res_obj, 'id', None)
 
         # self.assertIn(b'You were logged in', response.data)
         self.assertEqual(response.status_code, 200)
